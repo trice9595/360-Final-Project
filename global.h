a8f44ab9b43c5dbc850d54fdd2a30a252e3d62cd
trice9595 <timmyrice9595@gmail.com>
2016-11-18 00:21:24 -0800

    Added global.h to replace global.c

---
+++ global.h
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <ext2fs/ext2_fs.h>
+#include <string.h>
+#include <libgen.h>
+#include <sys/stat.h>
+
+
+#define FREE          0
+#define READY         1
+
+#define BLKSIZE     1024
+#define BLOCK_SIZE  1024
+
+#define NMINODE      100
+#define NFD           16
+#define NPROC          4
+
+typedef unsigned char  u8;
+typedef unsigned short u16;
+typedef unsigned int   u32;
+
+typedef struct ext2_super_block SUPER;
+typedef struct ext2_group_desc  GD;
+typedef struct ext2_inode       INODE;
+typedef struct ext2_dir_entry_2 DIR;
+
+extern SUPER *sp;
+extern GD    *gp;
+extern INODE *ip;
+extern DIR   *dp;
+
+extern MINODE minode[NMINODE];
+extern PROC   proc[NPROC], *running;
+extern MINODE *root;
+
+extern int dev;
+extern char buf[256];
+
