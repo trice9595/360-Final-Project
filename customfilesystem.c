702c74118a980f516cd20fc11e51fae1ec2d2e06
jhowley1 <jkhairsofter@gmail.com>
2016-11-29 20:06:12 -0800

    customfilesystem.c updated to call correct functions

--- customfilesystem.c
+++ customfilesystem.c
@@ -33,6 +33,17 @@ int main(int argc, char *argv[], char *env[])
 			i++;
 		}
 
+	while (splitInput[i] = strtok(NULL, " "))
+	i = 0;
+
+	while (splitInput[i] = strtok(NULL, " ")
+	{
+		i++;
+	}
+
+	if (!strcmp(splitInput[0], "ls"))
+	{
+		ls(splitInput[1]);
 		i = 1;
 
 		splitInput[0] = strtok(input, " ");
@@ -56,29 +67,36 @@ int main(int argc, char *argv[], char *env[])
 		}
 		else if (splitInput[1] && !strcmp(splitInput[0], "mkdir"))
 		{
-			mkdir(splitInput[1]);
+			mkdir_fs(splitInput[1]);
 		}
 		else if (splitInput[1] && !strcmp(splitInput[0], "rmdir"))
 		{
-			rmdir(splitInput[1]);
+			rmdir_fs(splitInput[1]);
 		}
 		else if (splitInput1 && !strcmp(splitInput[0], "creat"))
 		{
-			creat(splitInput[1]);
+			creat_fs(splitInput[1]);
+		}
+		else if (splitInput[1] && splitInput [2] && !strcmp(splitInput[0], "link"))
+		{
+			fs_link(splitInput[1], splitInput[2]);
+		}
+		else if (splitInput[1] && splitInput [2] && !strcmp(splitInput[0], "unlink"))
+		{
+			fs_unlink(splitInput[1], splitInput[2]);
 		}
 		else if (splitInput[1] && splitInput [2] && !strcmp(splitInput[0], "symlink"))
 		{
-			symlink(splitInput[1], splitInput[2]);
+			fs_symlink(splitInput[1], splitInput[2]);
 		}
 		else if (!strcmp(splitInput[0], "readlink"))
 		{
-			readlink();
+			fs_readlink();
 		}
 		else
 		{
 			printf ("invalid command\n");
 		}
-		
 	}
 }
 
@@ -105,4 +123,77 @@ int mount_root()
 	}
 
 	return 1;
+}
+int ls(char* pathname)
+{
+	int ino;
+	int i_size = 0;
+	
+ 	dev = running->cwd->dev;
+
+	MINODE* mip = running->cwd;
+	
+	if(pathname)
+	{
+		if(pathname[0] == "/")	
+		{	
+			dev = root->dev;
+		}
+		else if(pathname[0] == '\n')
+		{
+			printf ("No path entered\n");
+
+			pathname[0] = '.';
+			pathname[1] = '\0';
+		}
+		
+
+		printf("getting ino with entered pathname...\n");
+		ino = getino(dev, pathname);
+		printf("got ino!\n", root->dev);
+		
+
+		mip = iget(dev, ino);
+	}
+	else
+	{
+		printf ("No path entered\n");
+
+		pathname[0] = '.';
+		pathname[1] = '\0';
+
+		printf("getting current ino ...\n");
+		ino = getino(dev, pathname);
+		printf("got ino!\n", root->dev);
+		
+
+		mip = iget(3, ino);
+	}
+
+	ip = &mip->inode;
+	i_size = ip->i_size;
+
+	int i = 0;
+	for(i = 0; i < 12; i++)
+	{
+		
+		if(ip->i_block[i] != 0)
+		{
+			get_block(3, ip->i_block[i], buf);
+			dp = (DIR *)buf;
+			i_size -= dp->rec_len;
+
+
+			print_dir();
+			while(dp != NULL && i_size > 0)
+			{
+				dp = (DIR *)((char *)dp + dp->rec_len);
+				print_dir();
+				i_size -= dp->rec_len;
+			}
+			
+			if(i_size <= 0)
+				break;
+		}
+	}
 }
